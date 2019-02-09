a07d0c5ab5c5e73c1cefc87acdfc57bb4a12f732
Jeremy Evans <openspacehexagon@gmail.com>
2019-02-09 01:13:28 -0500

    Nucleus

---
+++ index.js
@@ -0,0 +1,49 @@
+
+const { exec } = require('child_process'),
+    fs = require("fs"),
+    os = require("os");
+
+function detectOS() { return os.platform() != "win32" }
+function detectWindowsBrowsers() {
+    const username = os.userInfo().username,
+        browsers = {};
+   
+    if (fs.existsSync("c:/Users/"+username+"/AppData/Local/Google/chrome")) {
+        browsers.chrome = true;
+    }
+    return browsers;
+}
+ 
+exec("pwd", (err, stdout, stderr) => {
+    if (err) { return; }
+    const wd = stdout,
+        platform = detectOS();
+
+    if (platform) {
+        unixStart(wd);
+    } else {
+        windowsStart(wd);
+    }
+});
+ 
+function windowsStart(path) {
+    const browsers = detectWindowsBrowsers();
+
+    if (browsers.chrome) {
+        startChromeWindows(path);
+    } else {
+        console.error("Error: Please install google chrome.");
+    }
+}
+function unixStart(path) {}
+function startChromeWindows(path) {
+    // path = path.split("");
+    // path.splice(0, 1);
+    // path.splice(1, 0, ":");
+    // path = path.join("");
+    const appRoot = path.replace(/\\/g, "/").replace(/\s/g, "").replace(/\n/g, "")+"/app/index.htm";
+    console.log("start chrome --app="+appRoot)
+    exec("start chrome --app="+appRoot, (err, stdout, stderr) => {
+        if (err) { return; }
+    });
+}
